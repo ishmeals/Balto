c83fc2cc8995a02f4e84292a359ce71bf16c207b
ishmeal <theoleehusky@gmail.com>
2024-12-11 02:15:26 +0000

    balto working

--- balto.py
+++ balto.py
@@ -2,6 +2,7 @@ import gymnasium as gym
 import numpy as np
 from gymnasium import spaces
 from functools import cache
+from math import ceil, log2
 
 @cache
 def factorial(n):
@@ -9,13 +10,6 @@ def factorial(n):
 
 def flatten(g):
     return [x for row in g for x in row]
-
-def encode(permutation):
-    n = len(permutation)
-    def parial_result(i):
-        return sum(j < permutation[i] for j in permutation[i:]) * factorial(n-i-1)
-
-    return sum(map(parial_result, range(n)))
 
 def decode(n, lehmer):
     result = [(lehmer % factorial(n-i)) // factorial(n-1-i) for i in range(n)]
@@ -31,46 +25,146 @@ def decode(n, lehmer):
                 break
     return result
 
-class CustomEnv(gym.Env):
+def encode(permutation):
+    n = len(permutation)
+    def parial_result(i):
+        return sum(j < permutation[i] for j in permutation[i:]) * factorial(n-i-1)
+
+    return sum(map(parial_result, range(n)))
+
+class Balto(gym.Env):
     """Custom Environment that follows gym interface."""
 
+    steps = 0
+    # hist = []
+
+    metadata = {'render_modes': ['human'], 'render_fps': 30}
+
     def done(self):
-        for i, x in enumerate(flatten(self.G)):
-            if i == self.N**2 and x != 0: return False
-            elif i < self.N**2 and x != i+1: return False
-            elif x != i+2: return False
-        return True
+        return self.goal == [self.G[p] for p in self.order]
 
-    # metadata = {"render_modes": ["human"], "render_fps": 30}
+    def encode(self):
+        i = encode([self.G[p] for p in self.order])
+        return [int(x) for x in bin(i)[2:].zfill(self.outlen)]
+        # return [self.G[p] for p in self.order]
 
-    def __init__(self, n):
-        super().__init__()
+    def __init__(self, n, render_mode="human"):
+        self.render_mode = render_mode
 
-        self.N = 3
-        SIZE = 3*n**2-3*n+1
+        self.N = n
+        self.SIZE = 3*n*(n-1)+1
 
-        self.G = [[15,13,8],
-                [14,17,9,16],
-                [10,18,0,4,12],
-                [3,7,2,5],
-                [11,6,1]]
+        self.order = []
+        l, r = 0, n-1
+        for j in range(-n+1, n):
+            for i in range(l, r+1):
+                self.order.append((i, j))
+            if j < 0: l -= 1
+            else: r -= 1
+        assert len(self.order) == self.SIZE
+
+        self.goal = [*range(1, self.SIZE//2+1)] + [0] + [*range(self.SIZE//2+1, self.SIZE)]
+        self.lookup = {i: p for (i, p) in zip(self.goal, self.order)}
+
+        # a = [15,13,8,
+        #         14,17,9,16,
+        #         10,18,0,4,12,
+        #         3,7,2,5,
+        #         11,6,1]
+        # self.G = {o: b for (o, b) in zip(self.order, a)}
+
         # Define action and observation space
         # They must be gym.spaces objects
         # Example when using discrete actions:
         self.action_space = spaces.Discrete(12)
+        self.outlen = int(ceil(log2(factorial(self.SIZE))))
         # Example for using image as input (channel-first; channel-last also works):
-        self.observation_space = spaces.Discrete(factorial(SIZE))
+        self.observation_space = spaces.MultiDiscrete([2]* self.outlen)
+
+    def cube_distance(self, a, b):
+        a = (*a, -a[0] - a[1])
+        b = (*b, -b[0] - b[1])
+        vec = (a[0] - b[0], a[1] - b[1], a[2] - b[2])
+        return max(map(abs, vec))
+
+    def cost(self):
+        return sum(self.cube_distance(p, self.lookup[i]) for p,i in self.G.items()) + self.steps/1000000 + 1
 
     def step(self, action):
-        ...
-        return observation, reward, terminated, truncated, info
+        self.steps += 1
+        # self.hist.append(action)
 
-    def reset(self, seed=None, options=None):
+        group = [p for p in self.G if self.G[p] == 0]
+        if action % 6 == 0:
+            group.append((group[0][0]+1, group[0][1]-1))
+            group.append((group[0][0]+1, group[0][1]))
+        elif action % 6 == 1:
+            group.append((group[0][0]+1, group[0][1]))
+            group.append((group[0][0], group[0][1]+1))
+        elif action % 6 == 2:
+            group.append((group[0][0], group[0][1]+1))
+            group.append((group[0][0]-1, group[0][1]+1))
+        elif action % 6 == 3:
+            group.append((group[0][0]-1, group[0][1]+1))
+            group.append((group[0][0]-1, group[0][1]))
+        elif action % 6 == 4:
+            group.append((group[0][0]-1, group[0][1]))
+            group.append((group[0][0], group[0][1]-1))
+        elif action % 6 == 5:
+            group.append((group[0][0], group[0][1]-1))
+            group.append((group[0][0]+1, group[0][1]-1))
+
+        for i, p in enumerate(group):
+            if p[0] < 0 and p[1] == self.N:
+                group[i] = (p[0]+self.N, -self.N+1)
+            elif p[0] > 0 and p[1] == -self.N:
+                group[i] = (p[0]-self.N, self.N-1)
+            elif sum(p) == self.N and p[1] > 0:
+                group[i] = (p[0]-self.N+1, p[1]-self.N)
+            elif sum(p) == -self.N and p[1] < 0:
+                group[i] = (p[0]+self.N-1, p[1]+self.N)
+            elif p[0] == self.N and p[1] > -self.N:
+                group[i] = (-self.N+1, p[1]+self.N-1)
+            elif p[0] == -self.N and p[1] < self.N:
+                group[i] = (self.N-1, p[1]-self.N+1)
+
+        if action < 6:
+            self.G[group[0]] = self.G[group[2]]
+            self.G[group[2]] = self.G[group[1]]
+            self.G[group[1]] = 0
+        else:
+            self.G[group[0]] = self.G[group[1]]
+            self.G[group[1]] = self.G[group[2]]
+            self.G[group[2]] = 0
+
+        d = self.c - self.cost()
+        self.c = self.cost()
+        return self.encode(), d, self.done(), False, {'cost': self.c}
+
+    def reset(self, a=None, seed=None, options=None):
+        # super().reset(seed=seed)
         assert seed is None and options is None
-        return encode(flatten(self.G))
+
+        if not a:
+            # a = range(self.SIZE)
+            a = np.random.permutation(self.SIZE)
+
+        self.G = {o: b for (o, b) in zip(self.order,a)}
+        self.c = self.cost()
+
+        return self.encode(), dict()
 
     def render(self):
-        print(self.G)
+        print(self.cost())
+        # print(self.hist)
 
     # def close(self):
     #     ...
+
+# env = Balto(3, render_mode="human")
+# env.reset(list(range(19)))
+# print(env.G)
+# print(env.cost())
+# env.step(0)
+# print(env.G)
+# print(env.cost())
